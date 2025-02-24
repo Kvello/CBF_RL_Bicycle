c712240372efdf36379990cd911d98d51916a9eb
Kvello <markus.kv1@gmail.com>
2025-02-24 15:04:47 +0100

    Finish integrator env with test

--- envs/integrator.py
+++ envs/integrator.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from typing import Union, NoneType
+from typing import Union, Optional, List
 from tensordict import TensorDict, TensorDictBase
 from torchrl.data.tensor_specs import (
     BoundedTensorSpec, 
@@ -27,26 +27,26 @@ class SafeDoubleIntegratorEnv(EnvBase):
         The input constraints are u in [-max_input, max_input]
     """
     rng = None
-    def __init__(self, td_params=None, seed=None, device=None,max_steps:int=100):
+    batch_locked = False
+    def __init__(self, td_params=None, seed=None, device=None):
         self.device = device
-        if td_params in None:
+        if td_params is None:
             td_params = self.gen_params(device=self.device)
         super().__init__(device=self.device)
         self._make_spec(td_params)
-        if seed is not None:
+        if seed is None:
             seed = torch.empty((), dtype=torch.int64, device=self.device).random_(
                 generator=self.rng).item()
         self.set_seed(seed)
-    def _set_seed(self, seed:int):
-        rng = torch.Generator(device=self.device)
-        rng.manual_seed(seed)
+    def _set_seed(self, seed: Optional[int]):
+        rng = torch.manual_seed(seed)
         self.rng = rng
     @staticmethod
-    def gen_params(batch_size:Union[int, NoneType]=None, device=None)->TensorDictBase:
+    def gen_params(batch_size:Optional[List[int]]=None, device=None)->TensorDictBase:
         """Returns a TensorDict with the parameters of the environment
 
         Args:
-            batch_size (Union[int, NoneType], optional): batch size. Defaults to None.
+            batch_size (int, optional): batch size. Defaults to None.
             device (string, optional): device to use. Defaults to None.
 
         Returns:
@@ -57,7 +57,6 @@ class SafeDoubleIntegratorEnv(EnvBase):
         td = TensorDict({
             "params": TensorDict({
                 "dt": 0.01,
-                "max_steps": 100,
                 "max_input": 1.0,
                 "max_x1": 1.0,
                 "max_x2": 1.0,
@@ -75,21 +74,14 @@ class SafeDoubleIntegratorEnv(EnvBase):
         Args:
             td_params (TensorDictBase): TensorDict with keys 'params'
         """
-        self.observation_spec = Composite(
-            x1 = BoundedTensorSpec(
-                low=-td_params["params", "max_x1"], 
-                high=td_params["params", "max_x1"],
+        self.observation_spec = CompositeSpec(
+            x1 = UnboundedContinuousTensorSpec(
                 shape=(),
-                dtype=torch.float64),
-            x2 = BoundedTensorSpec(
-                low=-td_params["params", "max_x2"], 
-                high=td_params["params", "max_x2"],
+                dtype=torch.float32),
+            x2 = UnboundedContinuousTensorSpec(
                 shape=(),
-                dtype=torch.float64),
-            params = make_composite_from_td(
-                td_params["params"],
-                unsqueeze_null_shapes=False
-            ),
+                dtype=torch.float32),
+            params = self.make_composite_from_td(td_params["params"]),
             shape=(),
         )
         self.state_spec = self.observation_spec.clone()
@@ -97,17 +89,25 @@ class SafeDoubleIntegratorEnv(EnvBase):
             low=-td_params["params", "max_input"],
             high=td_params["params", "max_input"],
             shape=(),
-            dtype=torch.float64
+            dtype=torch.float32
         )
+#        self.done_spec = CompositeSpec(
+#            done = DiscreteTensorSpec(
+#                shape  = torch.bool,
+#                n= (*td_params.shape,1),
+#                dtype=2),
+#            shape = ()
+#        )
+
         # TODO: This should be a discrete spec allowing only -1 and 0. This does not exist in the API
         # and a custom spec should be created
         self.reward_spec = BoundedTensorSpec(
             low=-1.0,
             high=0.0,
-            shape=(),
-            dtype=torch.float64
+            shape=(*td_params.shape,1),
+            dtype=torch.float32
         )
-    def make_composite_from_td(td:TensorDict):
+    def make_composite_from_td(self,td:TensorDict):
         composite = CompositeSpec(
             {
                 key : make_composite_from_td(tensor)
@@ -118,8 +118,10 @@ class SafeDoubleIntegratorEnv(EnvBase):
                     shape=tensor.shape
                 )
                 for key, tensor in td.items()
-            }
+            },
+            shape=td.shape
         )
+        return composite
     @classmethod
     def _step(cls, tensordict: TensorDict):
         """
@@ -127,14 +129,18 @@ class SafeDoubleIntegratorEnv(EnvBase):
             tensordict (TensorDict): dict with keys 'x1','x2' and 'action'
 
         Returns:
-            TensorDict: dict with keys 'x1' 'x2', 'reward', 'done', 'info'
+            TensorDict: dict with keys 'x1' 'x2', 'reward','terminated', 'info'
         """
         x1 = tensordict['x1']
         x2 = tensordict['x2']
         u = tensordict['action'].squeeze()
         u = torch.clamp(u, -1, 1)
+        # Note that here we use the state, not the next state for the cost
+        # That means that when an action makes the next state unsafe, this will
+        # only be detected in the next step, and we won't set termiated to True immeadiately
+        # This is a design choice, and can be changed if needed
         costs = torch.zeros_like(x1)
-        costs = torch.where(not cls.constraints_satisfied(x1, x2), torch.tensor(1.0), costs)
+        costs = torch.where(cls.constraints_satisfied(x1, x2), costs, torch.tensor(1.0))
 
         dt = tensordict["params", "dt"]
         # Unpack state and action
@@ -142,19 +148,21 @@ class SafeDoubleIntegratorEnv(EnvBase):
         x1_new = x1 + x2*dt + 0.5*u*dt**2
         x2_new = x2 + u*dt
 
+        # Done
+        terminated = costs > 0
+        done = terminated.clone()
         # Reward
         reward = -costs.view(*tensordict.shape,1)
-
-        # Done
-        done = torch.zeros_like(x1)
 
         out = TensorDict({
             "x1": x1_new,
             "x2": x2_new,
             "params": tensordict["params"],
             "reward": reward,
-            "done": done
-        })
+            "done": done,
+            "terminated": terminated,
+        },
+        tensordict.shape)
         return out
     def _reset(self,tensordict):
         batch_size = (
@@ -163,20 +171,25 @@ class SafeDoubleIntegratorEnv(EnvBase):
         if tensordict is None or tensordict.is_empty():
             tensordict = self.gen_params(batch_size = batch_size)
         x1 = (
-            torch.rand(tensordict.shape,generator=self.rng,device=self.device)
+            torch.rand(tensordict.shape,generator=self.rng,device=self.device,dtype=torch.float32)
             *(2*tensordict["params","max_x1"])
             - tensordict["params","max_x1"]
         )
         x2 = (
-            torch.rand(tensordict.shape,generator=self.rng,device=self.device)
+            torch.rand(tensordict.shape,generator=self.rng,device=self.device,dtype=torch.float32)
             *(2*tensordict["params","max_x2"])
             - tensordict["params","max_x2"]
         )
-        out = TensorDict({
+        terminated= torch.zeros(batch_size,dtype=torch.bool,device=self.device)
+        done = terminated.clone()
+        out = TensorDict(
+            {
             "x1": x1,
             "x2": x2,
-            "params": tensordict["params"]
-        })
+            "params": tensordict["params"],
+            },
+        batch_size=batch_size)
+        return out
     @classmethod
-    def constraints_satisfied(cls, x1:torch.Tensor, x2:torch.Tensor):
+    def constraints_satisfied(cls, x1:torch.Tensor, x2:torch.Tensor)->torch.Tensor:
         return (x1 >= -1) & (x1 <= 1) & (x2 >= -1) & (x2 <= 1)
--- shell.nix
+++ shell.nix
@@ -20,6 +20,13 @@ pkgs.mkShellNoCC {
       torch
       torchrl
       tensordict
+      pytest
+      ipykernel
+      pip
     ]))
   ];
+  shellHook = ''
+    export PYTHONPATH=$PWD
+    echo "Python environment ready. Run 'pytest tests/'"
+  '';
 }
