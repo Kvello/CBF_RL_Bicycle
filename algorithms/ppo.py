3586d58f26d1756712cf432fb0849f7ddbe93e4d
Kvello <markus.kv1@gmail.com>
2025-03-25 10:43:09 +0100

    Implements HiPPO(not tested)

--- algorithms/ppo.py
+++ algorithms/ppo.py
@@ -31,9 +31,6 @@ class HierarchicalPPO(RLAlgoBase):
         warn_str = "max_grad_norm not found in config, using default value of 1.0"
         self.max_grad_norm = get_config_value(config, "max_grad_norm", 1.0)
         
-        warn_str = "entropy_eps not found in config, using default value of 0.0"
-        self.entropy_eps = get_config_value(config, "entropy_eps", 0.0, warn_str)
-
         warn_str = "device not found in config, using default value of 'cpu'"
         self.device = get_config_value(config, "device", "cpu", warn_str)
 
@@ -54,6 +51,12 @@ class HierarchicalPPO(RLAlgoBase):
 
         warn_str = "num_epochs not found in config, using default value of 10"
         self.num_epochs = get_config_value(config, "num_epochs", 10, warn_str) 
+        
+        warn_str = "primary_reward_key not found in config, using default value of 'r1'"
+        self.primary_reward_key = get_config_value(config, "primary_reward_key", "r1", warn_str)
+        
+        warn_str = "secondary_reward_key not found in config, using default value of 'r2'"
+        self.secondary_reward_key = get_config_value(config, "secondary_reward_key", "r2", warn_str)
 
     def train(self,
               policy_module: TensorDictModule,
@@ -62,7 +65,7 @@ class HierarchicalPPO(RLAlgoBase):
               optim: torch.optim.Optimizer,
               collector: DataCollectorBase,
               replay_buffer: TensorDictReplayBuffer,
-              eval_func: Optional[Callable[None, Dict[str, float]]] = None
+              eval_func: Optional[Callable[None, Dict[str, float]]] = None,
               ):
         """Train the PPO algorithm.
 
@@ -94,7 +97,9 @@ class HierarchicalPPO(RLAlgoBase):
         else:
             raise ValueError("Collector must have total_frames attribute.\
                                 Try using a different collector.")
-
+        if V_primary.out_keys[0] == V_secondary.out_keys[0]:
+            warnings.warn("Value networks have the same output keys. This may cause issues.")
+            
         if self.config.get("track", False):
             wandb.init(project=self.config.get("wandb_project", "ppo"),
                     sync_tensorboard=True,
@@ -110,6 +115,12 @@ class HierarchicalPPO(RLAlgoBase):
             average_gae=True,
             device=self.device,
         )
+        self.A_primary.set_keys(
+            reward_key=self.primary_reward_key,
+            advantage_key="A1",
+            value_target_key="V1_target",
+            value_key=V_primary.out_keys[0]
+        )
         self.A_secondary = GAE(
             gamma=self.gamma,
             lmbda=self.lmbda,
@@ -117,28 +128,51 @@ class HierarchicalPPO(RLAlgoBase):
             average_gae=True,
             device=self.device,
         )
+        self.A_secondary.set_keys(
+            reward_key=self.secondary_reward_key,
+            advantage_key="A2",
+            value_target_key="V2_target",
+            value_key=V_secondary.out_keys[0]
+        )
 
+        # Could probably just write a custom loss function at this point, but 
+        # the torchrl implementation is fine for now
+        # Need to figure out if I should set separate_losses to True or not
         self.primary_loss = ClipPPOLoss(
             actor_network=policy_module,
             critic_network=value_module,
             clip_epsilon=self.clip_epsilon,
-            entropy_bonus=bool(self.entropy_eps),
-            entropy_coef=self.entropy_eps,
+            entropy_bonus=False,
+            entropy_coef=0.0,
             critic_coef=self.critic_coef,
             loss_critic_type=self.loss_critictype,
+        )
+        self.primary_loss.set_keys(
+            advantage_key="A1",
+            value_key=V_primary.out_keys[0],
+            value_target_key="V1_target",
+            reward_key=self.primary_reward_key,
         )
         self.secondary_loss = ClipPPOLoss(
             actor_network=policy_module,
             critic_network=value_module,
             clip_epsilon=self.clip_epsilon,
-            entropy_bonus=bool(self.entropy_eps),
-            entropy_coef=self.entropy_eps,
+            entropy_bonus=False,
+            entropy_coef=0.0,
             critic_coef=self.critic_coef,
             loss_critic_type=self.loss_critictype,
         )
+        self.secondary_loss.set_keys(
+            advantage_key="A2",
+            value_key=V_secondary.out_keys[0],
+            value_target_key="V2_target",
+            reward_key=self.secondary_reward_key,
+        )
 
-        self.optim = optim(self.loss_module.parameters(), **self.config.get("optim_kwargs", {}))
-
+        self.optim = optim(
+            list(self.primary_loss.parameters()) + list(self.secondary_loss.parameters()),
+            **self.config.get("optim_kwargs", {})
+        )
         print("Training with config:")
         print(self.config)
         logs = defaultdict(list)
@@ -146,9 +180,11 @@ class HierarchicalPPO(RLAlgoBase):
         pbar = tqdm(total=total_frames)
         for i, tensordict_data in enumerate(collector):
             logs.update(self.step(tensordict_data,
-                                   self.loss_module,
+                                   self.primary_loss,
+                                   self.secondary_loss,
+                                   self.A_primary,
+                                   self.A_secondary,
                                    self.optim,
-                                   self.advantage_module,
                                    replay_buffer,
                                    eval_func=eval_func))
             pbar.update(tensordict_data.numel())
@@ -169,64 +205,112 @@ class HierarchicalPPO(RLAlgoBase):
 
     def step(self, 
              tensordict_data: TensorDict,
-             loss_module: TensorDictModule,
+             primary_loss_module: TensorDictModule,
+             secondary_loss_module: TensorDictModule,
+             A_primary: GAE,
+             A_secondary: GAE,
              optim: torch.optim.Optimizer,
-             advantage_module: TensorDictModule,
              replay_buffer: TensorDictReplayBuffer,
              eval_func: Optional[Callable[None, Dict[str, float]]] = None):
 
         if self.config is None:
             raise ValueError("Setup must be called with a config before training")
         logs = defaultdict(list)
-        logs["loss_objective"] = 0.0
-        logs["loss_critic"] = 0.0
-        logs["loss_entropy"] = 0.0
+        logs["loss_primary_objective"] = 0.0
+        logs["loss_secondary_objective"] = 0.0
+        logs["loss_primary_critic"] = 0.0
+        logs["loss_secondary_critic"] = 0.0
         for _ in range(self.num_epochs):
-            advantage_module(tensordict_data.to(self.device))
+            A_primary(tensordict_data.to(self.device))
+            A_secondary(tensordict_data.to(self.device))
             data_view = tensordict_data.reshape(-1)
             replay_buffer.extend(data_view)
             for _ in range(self.frames_per_batch // self.sub_batch_size):
                 subdata = replay_buffer.sample(self.sub_batch_size).to(self.device)
-                state_value = loss_module.critic_network(subdata)["state_value"]
+                primary_value_key = primary_loss_module.critic_network.out_keys[0]
+                secondary_value_key = secondary_loss_module.critic_network.out_keys[0]
+                primary_value = primary_loss_module.critic_network(subdata)[primary_value_key]
+                secondary_value = secondary_loss_module.critic_network(subdata)[secondary_value_key]
+
                 with torch.no_grad():
-                    subdata["td_error"] = torch.abs(subdata["value_target"] - state_value)
+                    subdata["td_error"] = torch.abs(subdata["V1_target"] - primary_value)
                     replay_buffer.update_tensordict_priority(subdata)
-                critic_loss = torch.nn.HuberLoss(reduction='none')(state_value, subdata["value_target"])
-                advantage = subdata.get("advantage", None) # Store before modifying
-                # Allow for importance sampling of the samples
+                primary_critic_loss = torch.nn.HuberLoss(reduction='none')(primary_value, subdata["V1_target"])
+                secondary_critic_loss = torch.nn.HuberLoss(reduction='none')(secondary_value, subdata["V2_target"])
+                A1 = subdata.get("A1", None) # Store before modifying
+                # Allow for importance sampling of the samples weigthed on the primary critic td error
                 if "_weight" in subdata:
-                    critic_loss = (subdata["_weight"] * critic_loss).mean() 
+                    primary_critic_loss = (subdata["_weight"] * critic_loss).mean() 
+                    secondary_critic_loss = (subdata["_weight"] * secondary_critic_loss).mean()
                     # I think this is a valid way of weighting the actor loss
-                    subdata["advantage"] = subdata["advantage"]* subdata["_weight"]
+                    subdata["A1"] = subdata["A1"]* subdata["_weight"]
+                    subdata["A2"] = subdata["A2"]* subdata["_weight"]
                 else:
-                    critic_loss = critic_loss.mean()
-                loss_vals = loss_module(subdata)
-                loss_vals["loss_critic"] = critic_loss * self.critic_coef
-                if self.entropy_eps == 0.0:
-                    loss_vals["loss_entropy"] = torch.tensor(0.0).to(self.device)
-                loss_value = (
-                    loss_vals["loss_objective"]
-                    + loss_vals["loss_critic"]
-                    + loss_vals["loss_entropy"]
+                    primary_critic_loss = primary_critic_loss.mean()
+                    secondary_critic_loss = secondary_critic_loss.mean()
+                primary_loss_vals = primary_loss_module(subdata)
+                secondary_loss_vals = secondary_loss_module(subdata)
+
+
+
+                ############################################
+                # Policy gradient with projection
+                ############################################
+
+                # Primary objective loss gradient
+                primary_loss_vals["loss_objective"].backward()
+                grad_vec_primary_objective_loss = torch.cat(
+                    [p.grad.view(-1) for p in primary_loss_module.actor_net.parameters()]
+                ) 
+                primary_loss_module.actor_net.zero_grad()
+                # Secondary objective loss gradient
+                secondary_loss_vals["loss_objective"].backward()
+                grad_vec_secondary_objective_loss = torch.cat(
+                    [p.grad.view(-1) for p in secondary_loss_module.actor_net.parameters()]
                 )
-                logs["loss_objective"] += loss_vals["loss_objective"].item()
-                logs["loss_critic"] += loss_vals["loss_critic"].item()
-                logs["loss_entropy"] += loss_vals["loss_entropy"].item()
+                secondary_loss_module.actor_net.zero_grad()
+                # Projection of secondary objective loss gradient onto nullspace of
+                # primary objective loss gradient
+                secondary_proj =(
+                    torch.dot(grad_vec_secondary_objective_loss, grad_vec_primary_objective_loss)
+                    / torch.dot(grad_vec_primary_objective_loss, grad_vec_primary_objective_loss)
+                    * grad_vec_primary_objective_loss
+                )
+
+                policy_grad = (
+                    grad_vec_secondary_objective_loss - secondary_proj + grad_vec_primary_objective_loss
+                )
+                # Set gradient to the policy module
+                for p, g in zip(primary_loss_module.actor_net.parameters(), policy_grad):
+                    p.grad = g.view_as(p)
+
+                # Critic losses
+                primary_loss_vals["loss_critic"] = primary_critic_loss * self.critic_coef
+                secondary_loss_vals["loss_critic"] = secondary_critic_loss * self.critic_coef 
+                critic_loss = primary_loss_vals["loss_critic"] + secondary_loss_vals["loss_critic"]
+                critic_loss.backward()
+                 
                 
-                loss_value.backward()
                 # this is not strictly mandatory but it's good practice to keep
                 # your gradient norm bounded
-                torch.nn.utils.clip_grad_norm_(loss_module.parameters(), 
-                                               self.max_grad_norm)
+                # torch.nn.utils.clip_grad_norm_(loss_module.parameters(), 
+                #                                self.max_grad_norm)
                 optim.step()
                 optim.zero_grad()
+
+                logs["loss_primary_objective"] += primary_loss_vals["loss_objective"].item()
+                logs["loss_secondary_objective"] += secondary_loss_vals["loss_objective"].item()
+                logs["loss_primary_critic"] += primary_critic_loss.item()
+                logs["loss_secondary_critic"] += secondary_critic_loss.item()
                 # Restore the advantage. I assume the subdata is a reference to the replay
                 # buffer data, so we need to restore the original advantage values
-                subdata["advantage"] = advantage
-        logs["loss_objective"] /= self.num_epochs
-        logs["loss_critic"] /= self.num_epochs
-        logs["loss_entropy"] /= self.num_epochs
-        logs["reward"] = tensordict_data["next", "reward"].mean().item()
+                subdata["A1"] = A1
+        logs["loss_primary_objective"] /= self.num_epochs
+        logs["loss_secondary_objective"] /= self.num_epochs
+        logs["loss_primary_critic"] /= self.num_epochs
+        logs["loss_secondary_critic"] /= self.num_epochs
+        logs[self.primary_reward_key] = tensordict_data["next", self.primary_reward_key].mean().item()
+        logs[self.secondary_reward_key] = tensordict_data["next", self.secondary_reward_key].mean().item()
         logs["step_count(average)"] = tensordict_data["step_count"].to(torch.float32).mean().item()
         logs["lr"] = optim.param_groups[0]["lr"]
         if eval_func is not None:
