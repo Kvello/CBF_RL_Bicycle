6a39ed9e7ecff61fe7725f8c0789f524e985f132
Kvello <markus.kv1@gmail.com>
2025-04-08 17:08:19 +0200

    WIP

--- envs/adaptive_reset.py
+++ envs/adaptive_reset.py
@@ -1,39 +1,23 @@
 from torchrl.envs import EnvBase
-from torchr.date import ReplayBuffer, LazyTensorStorage
+from torchrl.data import ReplayBuffer, LazyTensorStorage
 from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
-from typing import Callable
+from typing import Callable, Optional
 from tensordict import TensorDict
 import torch
 
-class AdaptiveResetWrapper(EnvBase):
+class AdaptiveResetEnv(EnvBase):
     """Environment wrapper that biases initial states 
     using a buffer."""
     def __init__(self, 
-                 buffer_size:int,
-                 env,
-                 save_fn:Callable[TensorDict,bool], 
-                 buffer_fraction=0.5):
+                 buffer_size,
+                 buffer_fraction=0.5,
+                 **kwargs):
         super().__init__()
-        self.env = env
         self.buffer = ReplayBuffer(
-            storage=LazyTensorStorage(size=buffer_size),
+            storage=LazyTensorStorage(max_size=buffer_size),
             sampler=SamplerWithoutReplacement()
         )
         self.buffer_fraction = buffer_fraction
-        self.save_fn = save_fn
-    def _reset(self,tensordict:TensorDict)->TensorDict:
-        batch_size = (
-            tensordict.batch_size if tensordict is not None else self.batch_size
-        )
-        if tensordict is None or tensordict.is_empty():
-            tensordict = self.gen_params(batch_size = batch_size)
-        # Save to buffer any initial states that fulfill the condition
-        save_indcs = self.save_fn(tensordict)
-        self.buffer.extend(tensordict[save])
-        # Sample from buffer
-        num_buffer_samples = min(len(self.buffer), int(self.buffer_fraction*batch_size))
-        return out
-    def __getattr__(self, name):
-        """Automatically forward method calls to the wrapped environment."""
-        return getattr(self.env, name)
-    
+    def extend_initial_state_buffer(self,td:TensorDict):
+        """Extends the initial state buffer with the provided tensor dict."""
+        raise NotImplementedError("This method should be implemented in a subclass.")
