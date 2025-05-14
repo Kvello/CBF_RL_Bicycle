Tuning. Try to fix NaN in gradient error

algorithms/hippo.py
@@ -61,12 +61,17 @@ def gradient_projection(
         grad_vec_primary_loss = torch.cat(
             [p.grad.view(-1) for p in common_module.parameters()]
         ) 
+        if torch.isnan(grad_vec_primary_loss).any():
+            raise ValueError("NaN in primary loss gradient")
         common_module.zero_grad()
         # Secondary objective loss gradient
-        secondary_loss.backward()
+        with torch.autograd.set_detect_anomaly(True):
+            secondary_loss.backward()
         grad_vec_secondary_loss = torch.cat(
             [p.grad.view(-1) for p in common_module.parameters()]
         )
+        if torch.isnan(grad_vec_secondary_loss).any():
+            raise ValueError("NaN in secondary loss gradient")
         common_module.zero_grad()
         if torch.isclose(grad_vec_primary_loss.norm(),torch.tensor(0.0),atol=1e-10):
             # If primary loss gradient is zero, return secondary loss gradient
@@ -83,7 +88,11 @@ def gradient_projection(
             secondary_proj = grad_vec_secondary_loss - secondary_proj 
         else:
             secondary_proj = grad_vec_secondary_loss
+        if torch.isnan(secondary_proj).any():
+            raise ValueError("NaN in secondary loss projection")
         grad = secondary_proj + grad_vec_primary_loss
+        if torch.isnan(grad).any():
+            raise ValueError("NaN in combined gradient")
         return grad
 
 class HierarchicalPPO(PPO):
@@ -324,6 +333,10 @@ class HierarchicalPPO(PPO):
         for p in loss_module.actor_network.parameters():
             new_grad = policy_grad[last_param_idx:last_param_idx + p.data.numel()]
             new_grad = new_grad.view_as(p.data)
+            if torch.isnan(new_grad).any():
+                raise ValueError("NaN in gradient")
+            if torch.isinf(new_grad).any():
+                raise ValueError("Inf in gradient") 
             p.grad = new_grad
             last_param_idx += p.data.numel()
         # this is not strictly mandatory but it's good practice to keep

algorithms/losses/hippo_loss.py
@@ -100,6 +100,24 @@ class HiPPOLoss(LossModule):
             )*self.supervision_coef
         else:
             supervised_CDF_loss = torch.tensor(0.0, device=tensordict.device)
+        assert not torch.isnan(
+            primary_loss_vals["loss_objective"]
+        ).any(), "NaN in primary loss"
+        assert not torch.isnan(
+            secondary_loss_vals["loss_objective"]
+        ).any(), "NaN in secondary loss"
+        assert not torch.isnan(
+            primary_loss_vals["loss_critic"]
+        ).any(), "NaN in primary critic loss"
+        assert not torch.isnan(
+            secondary_loss_vals["loss_critic"]
+        ).any(), "NaN in secondary critic loss"
+        assert not torch.isnan(
+            primary_loss_vals["loss_entropy"]
+        ).any(), "NaN in primary entropy loss"
+        assert not torch.isnan(
+            secondary_loss_vals["loss_entropy"]
+        ).any(), "NaN in secondary entropy loss"
         td_out = TensorDict(
             {
                 "loss_safety_objective": primary_loss_vals["loss_objective"],
@@ -136,7 +154,8 @@ class HiPPOLoss(LossModule):
         if mask.sum() == 0:
             # No transitions where the agent violates the CDF constraint
             # Return a loss of 0
-            return torch.tensor(0.0, device=tensordict.device)
+            dummy = self.actor_network(tensordict)  # Just get something on the same graph
+            return torch.zeros(1, device=tensordict.device) * dummy["action"].sum()
         data = tensordict[mask]
         loss_vals = self.primary_loss(data)
         return loss_vals["loss_objective"]